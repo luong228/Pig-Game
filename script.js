4c3e343b3a361ed3ea1981087116622af4e19df2
luong228 <90311663+luong228@users.noreply.github.com>
2023-11-13 07:51:46 +0700

    Finish game

--- script.js
+++ script.js
@@ -12,22 +12,35 @@ const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
 
-let activePlayer = 0;
+let scores, activePlayer, currentScore, isPlaying;
 
-score1El.textContent = 0;
-score0El.textContent = 0;
-diceEl.classList.add('hidden')
 
-let currentScore = 0;
-
+const initializeGame = () => {
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    scores = [0, 0];
+    activePlayer = 0;
+    currentScore = 0;
+    isPlaying = true;
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+    diceEl.classList.add('hidden')
+}
 const setCurrentScore = (score) => {
-    if (score === 1)
-        toggleBackground()
-
-    currentScore = score === 1 ? 0 : (currentScore + score)
-
+    currentScore += score
     displayCurrentScore()
-
+}
+const setTotalScore = (score) => {
+    scores[activePlayer] = score
+    if (activePlayer === 0)
+        score0El.textContent = score
+    else {
+        score1El.textContent = score
+    }
 }
 const toggleBackground = () => {
     player0El.classList.toggle('player--active')
@@ -36,18 +49,44 @@ const toggleBackground = () => {
 const displayCurrentScore = () => {
     activePlayer === 0 ? current0El.textContent = currentScore : current1El.textContent = currentScore
 }
+const switchPlayer = () => {
+    currentScore = 0;
+    displayCurrentScore();
+    toggleBackground();
+    activePlayer = activePlayer === 1 ? 0 : 1;
+}
+
 btnRoll.addEventListener('click', () => {
+    if(!isPlaying)
+        return
     const dice = Math.trunc(Math.random() * 6) + 1;
 
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`
 
     if (dice === 1) {
-        setCurrentScore(dice)
-        activePlayer = activePlayer === 1 ? 0 : 1
-        currentScore = 0;
+        setTotalScore(0);
+        switchPlayer();
         return
     }
 
-    setCurrentScore(dice, activePlayer)
+    setCurrentScore(dice)
 })
+
+btnHold.addEventListener('click', () => {
+    const holdScore = currentScore + scores[activePlayer]
+    setTotalScore(holdScore)
+
+    if (scores[activePlayer] >= 100) {   
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--active')
+        isPlaying = false;
+        return
+    }
+    switchPlayer()
+})
+
+btnNew.addEventListener('click', initializeGame)
+
+initializeGame();
+
